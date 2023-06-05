7b52d6138bb4c4926c53893f67a9d06a9828a3d6
Christian Kallas <bkallas3@gmail.com>
2023-06-05 12:43:57 -0700

    completed exercise

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-console.log("Hello World!\n==========\n");
-
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+const searchButton = document.querySelector("#submitSearch");
+const searchInput = document.querySelector("#searchWord");
+const img = document.querySelector("#img");
+const feedback = document.querySelector("#paragraph");
+
+
+const apiKey= "EioowPzkpK23NP4jNbLR8442NoZxMpqL"
+
+function fetchGiphy(){
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${searchInput.value}`)
+    .then((response)=>{
+        const result = response.json()
+        console.log(result)
+        return result;
+    })
+    
+    .then((res)=>{
+        img.src = res.data.images.original.url;
+        searchInput.value = "";
+    })
+    .catch((err)=> {
+        console.log(err)
+        feedback.textContent = err.message;
+    })
+}
+
+searchButton.addEventListener("click", fetchGiphy);
